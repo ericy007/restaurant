cf58513a6283f948dd6d672d3b7492768915aaf5
Eric Yabit <eric.yabit@live.com>
2019-09-19 05:31:02 +0000

    Updated Create meal task and index

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,11 +1,27 @@
 class MealsController < ApplicationController
 
   def index
-    @meal = Meal.new
+    @meal = Meal.order("RANDOM()").first
   end
 
   def new
     @meal = Meal.new
+  end
+   
+
+
+
+
+  def create
+    @meal = Meal.create(meal_params)
+    redirect_to root_path
+  end
+
+  private
+
+
+  def meal_params
+    params.require(:meal).permit(:title, :description)
   end
 
 end
